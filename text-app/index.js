7386dfdda31602172154f52ea9cad7a1a1faa74f
partykasidit <kasidit.09@hotmail.com>
2020-02-16 14:58:01 +0700

    Add database

--- text-app/index.js
+++ text-app/index.js
@@ -1,20 +1,42 @@
-var PORT = process.env.PORT || 5000;
-var express = require('express');
-var app = express();
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const { pool } = require('./config')
 
-var http = require('http');
-var server = http.Server(app);
+const app = express()
 
-app.use(express.static('client'));
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(cors())
 
-server.listen(PORT, function() {
-  console.log('Text server running');
-});
+const getBooks = (request, response) => {
+  pool.query('SELECT * FROM books', (error, results) => {
+    if (error) {
+      throw error
+    }
+    response.status(200).json(results.rows)
+  })
+}
 
-var io = require('socket.io')(server);
+const addBook = (request, response) => {
+  const { author, title } = request.body
 
-io.on('connection', function(socket) {
-  socket.on('message', function(msg) {
-    io.emit('message', msg);
-  });
-});
+  pool.query('INSERT INTO books (author, title) VALUES ($1, $2)', [author, title], error => {
+    if (error) {
+      throw error
+    }
+    response.status(201).json({ status: 'success', message: 'Book added.' })
+  })
+}
+
+app
+  .route('/books')
+  // GET endpoint
+  .get(getBooks)
+  // POST endpoint
+  .post(addBook)
+
+// Start server
+app.listen(process.env.PORT || 3002, () => {
+  console.log(`Server listening`)
+})
