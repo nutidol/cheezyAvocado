231caab82ac43bdfa9ba51b7dba337e5753f5414
partykasidit <kasidit.09@hotmail.com>
2020-03-11 19:47:14 +0700

    add avocabot class

---
+++ server/classes/avocabot.js
@@ -0,0 +1,18 @@
+class Avocabot {
+    constructor(currentPosition, currentDirection) {
+      this.currentPosition = currentPosition;
+      this.currentDirection = currentDirection;
+    }
+    turnLeft() {
+    
+    }
+    turnRight() {
+
+    }
+    walkStraight() {
+
+    }
+    changeDirection() {
+
+    }
+}
