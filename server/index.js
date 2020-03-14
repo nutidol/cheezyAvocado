50580b9a84953e09b425f1edb23aea4d3bc4cae3
partykasidit <kasidit.09@hotmail.com>
2020-03-14 21:09:26 +0700

    heroku connection is fixed.

--- server/index.js
+++ server/index.js
@@ -15,8 +15,26 @@ app.use(cors())
 app.use(compression())
 app.use(helmet())
 
+//test database connection
+const { Client } = require('pg');
+
+const client = new Client({
+  connectionString: 'postgres://tlcswvmrgvubvv:deff11dcc21c92aa82a4df312217f258df67a10b0b1e9d3831042878475e9640@ec2-184-72-235-159.compute-1.amazonaws.com/df17ujlpbqu69t',
+  ssl: true,
+});
+
+client.connect();
+
+client.query('SELECT * FROM customer;', (err, res) => {
+  if (err) throw err;
+  for (let row of res.rows) {
+    console.log(JSON.stringify(row));
+  }
+  client.end();
+});
+
 const getCustomers = (request, response) => {
-  pool.query('SELECT * FROM Customer', (error, results) => {
+  pool.query('SELECT * FROM customer', (error, results) => {
     if (error) {
       throw error
     }
